032e1b46e0d9421225cd1470f075937d0fdec9bd
MizAndhre <andry3632@gmail.com>
2022-02-09 14:36:24 -0500

    Update NavBar

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -17,7 +17,7 @@ export const HeroesApp = () => {
 
    return (
       <AuthContext.Provider value={{ user, dispatch }}>
-         <AppRouter />;
+         <AppRouter />
       </AuthContext.Provider>
    );
 };
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -16,13 +16,24 @@ export const Navbar = () => {
    };
 
    return (
-      <nav className='navbar navbar-expand navbar-dark bg-dark'>
+      <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
          <div className='container-fluid'>
             <Link className='navbar-brand fw-bold text-danger' to='/'>
                Heroes
             </Link>
 
-            <div className='collapse navbar-collapse'>
+            <button
+               className='navbar-toggler'
+               type='button'
+               data-bs-toggle='collapse'
+               data-bs-target='#navbarNav'
+               aria-controls='navbarNav'
+               aria-expanded='false'
+               aria-label='Toggle navigation'>
+               <span className='navbar-toggler-icon'></span>
+            </button>
+
+            <div className='collapse navbar-collapse' id="navbarNav">
                <ul className='navbar-nav'>
                   <li className='nav-item'>
                      <NavLink className='nav-link nav-item' end to='/marvel'>
